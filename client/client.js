5598fc972a02c6f04f7984042a12f745856002c6
amamoon <akeefmamoon@gmail.com>
2020-03-17 13:06:50 +1100

    Client Files from Previous repo

---
+++ client/client.js
@@ -0,0 +1,69 @@
+
+const form = document.querySelector('form'); //grabbing an element on the page
+const loadingElement = document.querySelector('.loading')
+const API_URL = 'http://localhost:5000/tweets'
+const tweetsElement = document.querySelector('.tweets')
+
+
+loadingElement.style.display = ''; 
+
+listAllTweets();
+//Tweet Submission
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name')
+    const content = formData.get('content')
+
+    const tweet = {
+        name,
+        content,
+    };
+    form.style.display = 'none';
+    loadingElement.style.display = '';
+    
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(tweet),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+    .then(createdTweet => {
+        console.log(createdTweet)
+        form.reset();
+        setTimeout(() => {
+            form.style.display = '';
+        }, 3000); //unhide the form after 3 seconds
+        listAllTweets();    
+        loadingElement.style.display= 'none';
+    })
+});
+//Tweet Listing and Visibility in most recent order
+function listAllTweets(){
+    tweetsElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(tweets => {
+            tweets.reverse();
+            tweets.forEach(tweet => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = tweet.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = tweet.content;
+
+                const created = document.createElement('small')
+                created.textContent = new Date(tweet.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(created);
+
+                tweetsElement.appendChild(div);
+            });
+            loadingElement.style.display = 'none';
+        });
+};
